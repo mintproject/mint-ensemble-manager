0c1ced84755df40dc88d136244fc1fe8c7a8c08c
Maximiliano Osorio <maxiosorio@gmail.com>
2024-06-06 08:42:29 -0400

    submit: execution

--- src/api/api-v1/services/executionsKubernetesService.ts
+++ src/api/api-v1/services/executionsKubernetesService.ts
@@ -1,25 +1,23 @@
 import { getThread } from "../../../classes/graphql/graphql_functions";
 import { Thread } from "../../../classes/mint/mint-types";
-import { fetchMintConfig } from "../../../classes/mint/mint-functions";
-import { KeycloakAdapter } from "../../../config/keycloak-adapter";
 import { createResponse } from "./util";
+import { ModelThread } from "../../../classes/api";
+import { saveAndRunExecutionsKubernetes } from "../../../classes/kubernetes/prepare-execution";
+import { fetchMintConfig } from "../../../classes/mint/mint-functions";
 
 const executionsKubernetesService = {
-    async submitExecution(threadmodel: any) {
-        const prefs = await fetchMintConfig();
-        console.log("submitExecution", prefs.graphql.username, prefs.graphql.password);
-        KeycloakAdapter.signIn(prefs.graphql.username, prefs.graphql.password);
-
+    async submitExecution(threadmodel: ModelThread) {
         const thread: Thread = await getThread(threadmodel.thread_id); //.then((thread: Thread) => {
         if (thread) {
-                return createResponse(
-                    "success",
-                    "Thread " + threadmodel.thread_id + " submitted for execution !"
-                );
-        } else {
-            return createResponse("failure", "Thread " + threadmodel.thread_id + " not found !");
+            const prefs = await fetchMintConfig();
+            saveAndRunExecutionsKubernetes(thread, threadmodel.model_id, prefs);
+            return createResponse(
+                "success",
+                "Thread " + threadmodel.thread_id + " submitted for execution !"
+            );
         }
-    },
+        return createResponse("failure", "Thread " + threadmodel.thread_id + " not found !");
+    }
 };
 
 export default executionsKubernetesService;
