2e504b1d4178bb78587ef3da1e77a3c7618c64a9
Maximiliano Osorio <maxiosorio@gmail.com>
2025-05-19 14:27:07 -0400

    feat: update logsService to include authorization handling in log fetching
    - Modified fetchLog method to accept an authorization header for improved security.
    - Updated GET function in logs API to pass the authorization header when fetching logs.
    - Enhanced logsService interface to reflect changes in fetchLog method signature.

--- src/api/api-v1/services/logsService.ts
+++ src/api/api-v1/services/logsService.ts
@@ -3,14 +3,17 @@ import { Execution } from "../../../classes/mint/mint-types";
 import { fetchWingsRunLog } from "../../../classes/wings/wings-functions";
 import { fetchLocalRunLog } from "../../../classes/localex/local-execution-functions";
 import { fetchMintConfig } from "../../../classes/mint/mint-functions";
-import { KeycloakAdapter } from "../../../config/keycloak-adapter";
 import { createResponse } from "./util";
 import { getTokenFromAuthorizationHeader } from "@/utils/authUtils";
 import { TapisExecutionService } from "@/classes/tapis/adapters/TapisExecutionService";
 
 // ./api-v1/services/logsService.js
+export interface LogsService {
+    getAccessToken(authorizationHeader: string): string;
+    fetchLog(execution_id: string, authorizationHeader: string): Promise<string>;
+}
 
-const logsService = {
+const logsService: LogsService = {
     getAccessToken(authorizationHeader: string): string {
         const access_token = getTokenFromAuthorizationHeader(authorizationHeader);
         if (!access_token) {
@@ -21,16 +24,14 @@ const logsService = {
     async fetchLog(execution_id: string, authorizationHeader: string) {
         try {
             const prefs = await fetchMintConfig();
-            KeycloakAdapter.signIn(prefs.graphql.username, prefs.graphql.password);
-
             const ensemble: Execution = await getExecution(execution_id);
-            if (!ensemble.execution_engine || ensemble.execution_engine == "wings") {
+            if (!ensemble.execution_engine || prefs.execution_engine == "wings") {
                 const log = await fetchWingsRunLog(ensemble.runid, prefs);
                 return log;
-            } else if (ensemble.execution_engine == "localex") {
+            } else if (prefs.execution_engine == "localex") {
                 const log = fetchLocalRunLog(execution_id, prefs);
                 return log;
-            } else if (ensemble.execution_engine == "tapis") {
+            } else if (prefs.execution_engine == "tapis") {
                 const access_token = this.getAccessToken(authorizationHeader);
                 const tapisExecutionService = new TapisExecutionService(
                     access_token,
@@ -45,7 +46,7 @@ const logsService = {
             }
         } catch (error) {
             console.log(error);
-            return createResponse("failure", error);
+            return createResponse("failure", error.message);
         }
     }
 };
