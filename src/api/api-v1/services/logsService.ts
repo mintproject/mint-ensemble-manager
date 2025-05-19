aa9542fffc4204abbb3247832a2f0e215de3178f
Maximiliano Osorio <maxiosorio@gmail.com>
2025-05-19 12:53:35 -0400

    feat: enhance logsService to support Tapis job history retrieval
    - Added getAccessToken method to extract and validate access tokens from authorization headers.
    - Updated fetchLog method to handle Tapis execution engine, retrieving job history and formatting logs accordingly.
    - Introduced getJobHistory method in TapisExecutionService to fetch job history from Tapis API.

--- src/api/api-v1/services/logsService.ts
+++ src/api/api-v1/services/logsService.ts
@@ -5,11 +5,20 @@ import { fetchLocalRunLog } from "../../../classes/localex/local-execution-funct
 import { fetchMintConfig } from "../../../classes/mint/mint-functions";
 import { KeycloakAdapter } from "../../../config/keycloak-adapter";
 import { createResponse } from "./util";
+import { getTokenFromAuthorizationHeader } from "@/utils/authUtils";
+import { TapisExecutionService } from "@/classes/tapis/adapters/TapisExecutionService";
 
 // ./api-v1/services/logsService.js
 
 const logsService = {
-    async fetchLog(execution_id: string) {
+    getAccessToken(authorizationHeader: string): string {
+        const access_token = getTokenFromAuthorizationHeader(authorizationHeader);
+        if (!access_token) {
+            throw new Error("Invalid authorization header");
+        }
+        return access_token;
+    },
+    async fetchLog(execution_id: string, authorizationHeader: string) {
         try {
             const prefs = await fetchMintConfig();
             KeycloakAdapter.signIn(prefs.graphql.username, prefs.graphql.password);
@@ -20,6 +29,18 @@ const logsService = {
                 return log;
             } else if (ensemble.execution_engine == "localex") {
                 const log = fetchLocalRunLog(execution_id, prefs);
+                return log;
+            } else if (ensemble.execution_engine == "tapis") {
+                const access_token = this.getAccessToken(authorizationHeader);
+                const tapisExecutionService = new TapisExecutionService(
+                    access_token,
+                    prefs.tapis.basePath
+                );
+                const response = await tapisExecutionService.getJobHistory(execution_id);
+                let log = "";
+                for (const result of response.result) {
+                    log += `[${result.created} - ${result.event}] ${result.description}\n`;
+                }
                 return log;
             }
         } catch (error) {
