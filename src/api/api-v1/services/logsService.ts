4f2a82191baa486f4bd2a3b916ec22d07161c4f3
Maximiliano Osorio <maxiosorio@gmail.com>
2025-06-16 12:34:37 -0400

    fix: improve error handling in logs API and logsService
    - Enhanced error handling in the logs API to differentiate between HttpError instances and general errors, providing more accurate HTTP status responses.
    - Updated logsService to throw a NotFoundError when an execution is not found, improving clarity in error reporting and ensuring consistent error management.

--- src/api/api-v1/services/logsService.ts
+++ src/api/api-v1/services/logsService.ts
@@ -6,6 +6,8 @@ import { fetchMintConfig } from "@/classes/mint/mint-functions";
 import { createResponse } from "./util";
 import { getTokenFromAuthorizationHeader } from "@/utils/authUtils";
 import { TapisExecutionService } from "@/classes/tapis/adapters/TapisExecutionService";
+import { HttpError } from "@kubernetes/client-node/dist";
+import { NotFoundError } from "@/classes/common/errors";
 
 // ./api-v1/services/logsService.js
 export interface LogsService {
@@ -22,32 +24,30 @@ const logsService: LogsService = {
         return access_token;
     },
     async fetchLog(execution_id: string, authorizationHeader: string) {
-        try {
-            const prefs = await fetchMintConfig();
-            const execution: Execution = await getExecution(execution_id);
-            if (!execution.execution_engine || prefs.execution_engine == "wings") {
-                const log = await fetchWingsRunLog(execution.runid, prefs);
-                return log;
-            } else if (prefs.execution_engine == "localex") {
-                const log = fetchLocalRunLog(execution_id, prefs);
-                return log;
-            } else if (prefs.execution_engine == "tapis") {
-                const access_token = this.getAccessToken(authorizationHeader);
-                const tapisExecutionService = new TapisExecutionService(
-                    access_token,
-                    prefs.tapis.basePath
+        const prefs = await fetchMintConfig();
+        const execution: Execution = await getExecution(execution_id);
+        if (!execution) {
+            throw new NotFoundError("Execution not found");
+        }
+        if (prefs.execution_engine == "wings") {
+            const log = await fetchWingsRunLog(execution.runid, prefs);
+            return log;
+        } else if (prefs.execution_engine == "localex") {
+            const log = fetchLocalRunLog(execution_id, prefs);
+            return log;
+        } else if (prefs.execution_engine == "tapis") {
+            const access_token = this.getAccessToken(authorizationHeader);
+            const tapisExecutionService = new TapisExecutionService(
+                access_token,
+                prefs.tapis.basePath
+            );
+            if (!execution.runid) {
+                return createResponse(
+                    "failure",
+                    "Run ID is not set for this execution. Please try again later."
                 );
-                if (!execution.runid) {
-                    return createResponse(
-                        "failure",
-                        "Run ID is not set for this execution. Please try again later."
-                    );
-                }
-                await tapisExecutionService.getLog(execution.runid);
             }
-        } catch (error) {
-            console.log(error);
-            return createResponse("failure", error.message);
+            await tapisExecutionService.getLog(execution.runid);
         }
     }
 };
